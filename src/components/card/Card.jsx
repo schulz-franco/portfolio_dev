705ba606c133f40705760ab8bab8a1221367b0ad
schulz-franco <fschulz2222@gmail.com>
2023-03-18 11:18:19 -0300

    Hovers

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -12,6 +12,7 @@ const Card = () => {
                     <h2>{data.name}</h2>
                     <h4>{data.profile}</h4>
                     <p>{data.text}</p>
+                    <p className="cv">Mirá mi currículo haciendo click <a href={data.cv}>aquí.</a></p>
                     <div className="social">
                         <a href={data.instagram}><img width={26} height={26} src="/assets/social/instagram.png" alt="Instagram"/></a>
                         <a href={data.linkedin}><img width={26} height={26} src="/assets/social/linkedin.png" alt="Linkedin"/></a>
